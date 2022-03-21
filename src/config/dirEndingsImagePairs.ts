cff90b6e0ea0debc4665ccb329cdf60d9d8c77be
Jo Te <email@jo-te.me>
2022-03-21 10:27:15 +0000

    Configure new file and folder icons

--- src/config/dirEndingsImagePairs.ts
+++ src/config/dirEndingsImagePairs.ts
@@ -2,4 +2,5 @@ import { create, EndingsImagePair } from "../EndingsImagePair";
 
 export const DIR_ENDINGS_IMAGE_PAIRS: EndingsImagePair[] = [
   create("src", "src-folder"),
+  create(".git", "git-folder"),
 ];
--- src/config/fileEndingsImagePairs.ts
+++ src/config/fileEndingsImagePairs.ts
@@ -7,6 +7,7 @@ export const FILE_ENDINGS_IMAGE_PAIRS: EndingsImagePair[] = [
   create(".ts", "ts-file"),
   create(".spec.ts", "spec-ts-file"),
   create(".tsx", "tsx-file"),
+  create(".tsbuildinfo", "tsbuildinfo-file"),
   create(".gitignore", "git-file"),
   create(
     [
@@ -22,4 +23,6 @@ export const FILE_ENDINGS_IMAGE_PAIRS: EndingsImagePair[] = [
   create([".scss", ".sass"], "sass-file"),
   create("package.json", "node-package-file"),
   create("package-lock.json", "node-package-lock-file"),
+  create(".json", "json-file"),
+  create(".md", "markdown-file"),
 ];
